044a1082732ec8f895f3fd29c32ddb916f7b5840
Egemen Göl <egemengol@gmail.com>
2022-03-12 18:55:21 +0300

    Implement burn, clean imports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { TOKEN_PROGRAM_ID } from "@solana/spl-token";
-import { clusterApiUrl, Connection, Keypair, PublicKey } from "@solana/web3.js"
+import { createBurnInstruction, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { clusterApiUrl, Connection, Keypair, PublicKey, sendAndConfirmTransaction, Transaction } from "@solana/web3.js"
 import { Metadata } from "@metaplex-foundation/mpl-token-metadata";
 import bs58 from "bs58";
 import { program } from 'commander';
@@ -17,35 +17,32 @@ const doesMetadataExist = async (conn: Connection, mint: PublicKey): Promise<boo
 
 async function getNFTs(conn: Connection, owner: PublicKey): Promise<{ address: PublicKey, mint: PublicKey }[]> {
     const tokenAccounts = await conn.getParsedTokenAccountsByOwner(owner, { programId: TOKEN_PROGRAM_ID })
-    const potentialNFTs: { address: PublicKey, mint: PublicKey }[] = []
 
-    // console.log(tokenAccounts.value.map(obj => {
-    //     const parsed = obj.account.data.parsed;
-    //     return [parsed.info.mint, parsed.info.tokenAmount]
-    // }))
+    const potentials = tokenAccounts.value
+        .filter(tokenAcc => tokenAcc.account.data.parsed.info.tokenAmount.amount === '1')
+        .map(tokenAcc => ({
+            address: tokenAcc.pubkey,
+            mint: new PublicKey(tokenAcc.account.data.parsed.info.mint),
+        }))
 
-    for (const tokenAcc of tokenAccounts.value) {
-        const parsed = tokenAcc.account.data.parsed;
-        if (parsed && parsed.info && parsed.info.tokenAmount.amount === '1') {
-            potentialNFTs.push({
-                address: tokenAcc.pubkey,
-                mint: new PublicKey(parsed.info.mint),
-            })
-        }
-    }
-    console.log('potential:', potentialNFTs.map(nft => nft.mint.toString()))
-
-    const metadataPullers = potentialNFTs.map(nft => doesMetadataExist(conn, nft.mint))
+    const metadataPullers = potentials.map(nft => doesMetadataExist(conn, nft.mint))
     const existingMetadatas = await Promise.all(metadataPullers);
 
-    return potentialNFTs.filter((nft, i) => existingMetadatas[i])
+    return potentials.filter((nft, i) => existingMetadatas[i])
 }
 
-async function burnNFTs() {
+async function burnNFTs(conn: Connection, owner: Keypair, nfts: { address: PublicKey, mint: PublicKey }[], limit: number) {
+    const tx = new Transaction()
 
+    for (const nft of nfts.slice(0, limit)) {
+        tx.add(createBurnInstruction(nft.address, nft.mint, owner.publicKey, 1, undefined, TOKEN_PROGRAM_ID))
+    }
+    return await sendAndConfirmTransaction(conn, tx, [owner])
 }
 
-const conn = new Connection(clusterApiUrl('mainnet-beta'))
+/// CLI
+
+const conn = new Connection(clusterApiUrl('testnet'))
 const owner: Keypair = Keypair.fromSecretKey(
     bs58.decode(process.env.PRIVATE_KEY!)
 );
@@ -53,11 +50,22 @@ console.log('Owner:', owner.publicKey.toString())
 
 async function nftPrinter() {
     const nfts = await getNFTs(conn, owner.publicKey)
-    console.log('metaplexes:', nfts.map(nft => nft.mint.toString()))
+    console.log('nfts:', nfts.map(nft => nft.mint.toString()))
+}
+
+async function nftBurner() {
+    const nfts = await getNFTs(conn, owner.publicKey)
+    console.log('nfts:', nfts.map(nft => nft.mint.toString()))
+    const limit = Number(process.env.LIMIT || '0')
+    if (limit > 0 && nfts.length > 0) {
+        console.log(await burnNFTs(conn, owner, nfts, limit))
+    } else {
+        console.log('limit:', limit, 'nft count:', nfts.length, 'No action.')
+    }
 }
 
 (async () => {
-    program.command('list')
-        .action(nftPrinter)
+    program.command('listnfts').action(nftPrinter)
+    program.command('burn').action(nftBurner)
     await program.parseAsync(process.argv)
 })();
